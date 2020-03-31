9549e793297b86706122adc6d9ae0a02c4a8c36f
tnanhekhan <1631995@gmail.com>
2020-03-31 02:15:07 +0200

    almost finalized pleasurable

---
+++ public/js/script-directions.js
@@ -0,0 +1,58 @@
+const directionContainer = document.getElementById("direction-container");
+const elementBeforeToggleButton = document.getElementById("directions-message");
+const steps = document.getElementsByClassName("step");
+const stepper = document.getElementsByClassName("stepper")[0];
+
+
+directionContainer.style.display = window.getComputedStyle(directionContainer).display;
+
+if (directionContainer.style.display === "flex") {
+    addStepScrollPositions();
+    stepper.style.display = "block";
+    elementBeforeToggleButton.insertAdjacentHTML("afterend", "<button class=\"view-button\">TOGGLE LIST VIEW</button>")
+} else if (directionContainer.style.display === "block") {
+    stepper.style.display = "none";
+    elementBeforeToggleButton.insertAdjacentHTML("afterend", "<button class=\"view-button\">TOGGLE SWIPE VIEW</button>")
+}
+
+directionContainer.onscroll = function () {
+    for (let i = 0; i < steps.length; i++) {
+        if (steps[i].dataset.scrollPosition > directionContainer.scrollLeft) {
+            steps[i].style.backgroundColor = "lightgrey";
+        } else {
+            steps[i].style.backgroundColor = "#2ecc71";
+        }
+    }
+};
+
+document.addEventListener("click", event => {
+    if (event.target && event.target.className === "view-button") {
+        if (directionContainer.style.display === "flex") {
+            directionContainer.style.display = "block";
+            event.target.innerText = "TOGGLE SWIPE VIEW";
+            stepper.style.display = "none";
+        } else if (directionContainer.style.display === "block") {
+            directionContainer.style.display = "flex";
+            event.target.innerText = "TOGGLE LIST VIEW";
+            addStepScrollPositions();
+            stepper.style.display = "block";
+        }
+    }
+
+    if (event.target && event.target.tagName === "ARTICLE") {
+        event.target.scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
+    }
+});
+
+
+function addStepScrollPositions() {
+    const scrollPositionMultiplier = Math.floor((directionContainer.scrollWidth - directionContainer.clientWidth) / steps.length);
+    for (let i = 0; i < steps.length; i++) {
+        steps[i].dataset.scrollPosition = `${scrollPositionMultiplier * i}`;
+        steps[i].addEventListener("click", event => {
+            directionContainer.scrollTo({left: event.target.dataset.scrollPosition, behavior: "smooth"})
+        });
+    }
+}
+
+
---
+++ public/js/script-index.js
@@ -0,0 +1,16 @@
+const locationForm = document.getElementById("location-form");
+const locationInput = document.getElementById("location-input");
+locationForm.insertAdjacentHTML("afterend", "<h2> OR </h2>\n <button class=\"location-button\">GET LOCATION FROM DEVICE</button>\n");
+
+document.addEventListener("click", event => {
+    if (event.target && event.target.className === "location-button") {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(position => {
+                locationInput.value = `${position.coords.latitude}, ${position.coords.longitude}`;
+                console.log(position)
+            });
+        } else {
+            alert("Geolocation is not supported by this browser.");
+        }
+    }
+});
